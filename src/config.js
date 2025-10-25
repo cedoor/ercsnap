d9d98f42f32cf7d88ee378a9bb8fdead53bfa93f
Cedoor <me@cedoor.dev>
2025-10-25 20:53:19 +0100

    chore: remove dotenv

--- src/config.js
+++ src/config.js
@@ -1,7 +1,3 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
 export const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 export const ETHERSCAN_API_URL = "https://api.etherscan.io/v2/api";
 export const DEFAULT_CHAIN_ID = 1; // Mainnet
